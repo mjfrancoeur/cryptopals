6e51ee327e2ead8627bccf923ee21b537d5e1825
Michael Francoeur <mjfrancoeur@gmail.com>
2017-06-28 09:12:16 -0400

    minor edits

--- 01_hexToBase64.js
+++ 01_hexToBase64.js
@@ -1,12 +1,25 @@
+// Convert hex string into a base-64 string
+function hexToB64(hex) {
+  let bin = hexToBin(hex);
+  let b64 = binToB64(bin);
+
+  return b64;
+}
+
 
 // Convert hex string into a binary string
+// Update function to take capitalized letters
 function hexToBin(hexStr) {
-  let bin = '';
   let hexConverter = '0123456789abcdef'.split('');
 
   // Convert hex string to an array of decimal strings
   let decArr = hexStr.split('').map( (el) => {
-    return hexConverter.indexOf(el);
+    let dec = hexConverter.indexOf(el.toLowerCase());
+    if (dec === -1) {
+      throw new RangeError(`${el} is not a hex digit`);
+    }
+
+    return dec;
   });
 
   // Convert array of decimal strings into a binary string
@@ -38,14 +51,16 @@ function binToB64(binStr) {
 
   // Divide binary string into 6 bit chunks
   let arrOfBits = divideIntoBits(binStr, 6);
+  // turn array of 6 bit binary strings into a Base 64 string
+  return fromBinToB64(arrOfBits).join('');
 
-  let arrOfDecs = fromBinToB64(arrOfBits);
 }
 
-// Converts an array of binary strings into an array of decimals
+// Converts an array of binary strings into an array of base 64 strings
 function fromBinToB64(arrBin) {
   let ruler = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01223456789+/'.split('');
-  return arrBin.map( (bin) {
+  return arrBin.map( (bin) => {
+    // Function under construction
     let dec = fromBinToDec(bin);
     return ruler[dec];
   });
@@ -53,9 +68,19 @@ function fromBinToB64(arrBin) {
 
 // Takes in a binary string and returns a decimal number
 function fromBinToDec(binStr) {
-  
+  let bin = Number(binStr);
+  let remainder = null;
+  let result = 0;
+
+  for (let i = 0; bin; i++ ) {
+    remainder = bin % 10;
+    bin = Math.floor(bin / 10);
+    result += remainder * Math.pow(2, i);
+  }
+  return result;
 }
 
+// Divide a binary string into an array of binary strings numBits long
 function divideIntoBits(str, numBits) {
   let bin = str.split('');
   let arrOfBits = [];
@@ -71,6 +96,7 @@ function divideIntoBits(str, numBits) {
   return arrOfBits;
 }
 
+// Add padding to binary strings that need additional bits
 function padBinString(binStr, num) {
 
   while (binStr.length < num) {
@@ -78,3 +104,7 @@ function padBinString(binStr, num) {
   }
 
   return binStr;
+}
+
+// test
+//console.log(hexToB64('f'));
